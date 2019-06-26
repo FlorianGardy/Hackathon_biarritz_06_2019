aacab717f7903daf23d51af82111150d5a923630
BB5014 <bblabriz@bbox.fr>
2019-06-26 19:41:39 +0200

    added a navbar, a footer a bootstrap container

--- front/hackathon_06/src/AppRouter.jsx
+++ front/hackathon_06/src/AppRouter.jsx
@@ -4,16 +4,23 @@ import Home from "./pages/Home.page";
 import Matches from "./pages/Matches.page";
 import Campuses from "./pages/Campuses.page";
 import Wilders from "./pages/Wilders.page";
+import { Container } from "reactstrap";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function AppRouter() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/campuses" exact component={Campuses} />
-        <Route path="/matches" exact component={Matches} />
-        <Route path="/campuses/:campus" exact component={Wilders} />
-      </Switch>
+      <Container fluid className="bg-warning vh-100">
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/campuses" exact component={Campuses} />
+          <Route path="/matches" exact component={Matches} />
+          <Route path="/campuses/:campus" exact component={Wilders} />
+        </Switch>
+        <Footer />
+      </Container>
     </Router>
   );
 }
---
+++ front/hackathon_06/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Nav } from "reactstrap";
+
+function Footer() {
+  return (
+    <>
+      <Nav className="fixed-bottom navbar-light bg-light">
+        Florian - Jonhattan - Perrine - Nathalie - Stéphane
+      </Nav>
+    </>
+  );
+}
+export default Footer;
