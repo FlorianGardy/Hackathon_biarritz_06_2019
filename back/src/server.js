dda7c356b6a0e705413c98c99d9383194608297f
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-06-26 18:51:22 +0200

    Initializing Sequelize

--- back/src/server.js
+++ back/src/server.js
@@ -1,17 +1,29 @@
 "use strict";
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
 
+const server = Hapi.server({
+  port: 3030,
+  host: "localhost"
+});
+
+server.route(require("./routes/campuses.routes"));
+server.route(require("./routes/matches.routes"));
+server.route(require("./routes/wilders.routes"));
+
 const init = async () => {
-  const server = Hapi.server({
-    port: 3030,
-    host: "localhost"
-  });
-
-  server.route(require("./routes/campuses.routes"));
-  server.route(require("./routes/matches.routes"));
-  server.route(require("./routes/wilders.routes"));
-
+  const sequelize = require("../db/connect");
+  sequelize.sync();
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Connection has been established successfully.");
+    })
+    .catch(err => {
+      console.error("Unable to connect to the database:", err);
+    });
   await server.start();
   console.log("Server running on %s", server.info.uri);
 };
