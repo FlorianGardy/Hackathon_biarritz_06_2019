36827df38174ddf4dd95c0e515fca897252947c2
Florian Gardy <florian.gardy@gmail.com>
2019-06-26 21:22:32 +0200

    Add db functions calls in routes

---
+++ back/src/db/wilder/wilder.actions.js
@@ -0,0 +1,12 @@
+// Update wilders (from API data)
+function updateWilders() {
+  // Update wilders process
+}
+
+// Get all wilders from DB
+function getWildersByCampus(campusId) {
+  const wilders = [];
+  return wilders;
+}
+
+module.exports = { updateWilders, getWildersByCampus };
--- back/src/routes/campuses.routes.js
+++ back/src/routes/campuses.routes.js
@@ -1,21 +1,37 @@
+const { updateCampuses, getCampuses } = require("../db/campus/campus.actions");
+const {
+  updateWilders,
+  getWildersByCampus
+} = require("../db/wilder/wilder.actions");
+const { updateMatches, calculateElos } = require("../db/match/match.actions");
+
 module.exports = [
   {
     method: "GET",
     path: "/campuses",
-    handler: (request, h) => {
-      const campuses = [];
-      return campuses;
+    handler: async (request, h) => {
+      try {
+        await updateCampuses();
+        await updateMatches();
+        await calculateElos();
+        return await getCampuses();
+      } catch (error) {
+        console.log(error);
+      }
     }
   },
   {
     method: "GET",
     path: "/campuses/{campusId}/wilders",
-    handler: (request, h) => {
-      const campusId = request.params.campusId;
-      console.log(campusId);
-
-      const wilders = [];
-      return wilders;
+    handler: async (request, h) => {
+      try {
+        const campusId = request.params.campusId;
+        await updateCampuses();
+        await updateWilders();
+        return getWildersByCampus(campusId);
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 ];
