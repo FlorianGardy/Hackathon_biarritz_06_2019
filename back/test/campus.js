6c7ea3875b728cff7e574af1424d3f0902c3346b
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-06-27 18:23:56 +0200

    add campuses/:id/wilders route test

--- back/test/campus.js
+++ back/test/campus.js
@@ -1,6 +1,7 @@
 const should = require("should");
 const { init } = require("../src/server");
 const { Campus } = require("../src/db/campus/campus.model");
+const { Wilder } = require("../src/db/wilder/wilder.model");
 
 describe("# Campus", () => {
   let server;
@@ -62,6 +63,48 @@ describe("# Campus", () => {
       });
       const payload = JSON.parse(res.payload);
       should(payload).match([campus1, campus2]);
+    });
+  });
+  // ##########################################################################################################
+  describe("## GET /campuses/:id/wilders", () => {
+    it("responds with 200", async () => {
+      const res = await server.inject({
+        method: "GET",
+        url: "/campuses/bia/wilders"
+      });
+      should(res.statusCode).equal(200);
+    });
+
+    it("responds an empty wilder list", async () => {
+      const res = await server.inject({
+        method: "GET",
+        url: "/campuses/bia/wilders"
+      });
+      const payload = JSON.parse(res.payload);
+      should(payload).deepEqual([]);
+    });
+
+    it("responds an wilders list with two wilder", async () => {
+      const wilder1 = {
+        id: 1,
+        firstname: "jonathan",
+        lastname: "diniz",
+        campus: "bia"
+      };
+      const wilder2 = {
+        id: 2,
+        firstname: "jonathan",
+        lastname: "diniz",
+        campus: "bia"
+      };
+      await Wilder.create(wilder1);
+      await Wilder.create(wilder2);
+      const res = await server.inject({
+        method: "GET",
+        url: "/campuses/bia/wilders"
+      });
+      const payload = JSON.parse(res.payload);
+      should(payload).match([wilder1, wilder2]);
     });
   });
 });
