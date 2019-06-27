63e7c2a2640b47d96e7e69fa50d0e258947246ad
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-06-27 09:32:23 +0200

    function to implant campus in db if not already in

--- back/src/db/campus/campus.actions.js
+++ back/src/db/campus/campus.actions.js
@@ -3,11 +3,19 @@ const getCampusesFromAPI = require("../../API/getCampusFromAPI");
 
 // refresh campuses (from API data)
 async function refreshCampuses() {
-  const campusFromApi = await getCampusesFromAPI();
-  const campusFromDb = await Campus.findAll();
-  campusFromApi.data.map(async campusAPI => {
-    if (campusFromDb.length === 0) {
-      await Campus.create(campusAPI);
+  const campusesFromApi = await getCampusesFromAPI();
+  const campusesFromDb = await Campus.findAll();
+  campusesFromApi.data.map(async campusAPI => {
+    let found = false;
+    for (let i = 0; i < campusesFromDb.length; i++) {
+      if (campusesFromDb[i].uid === campusAPI.uid) {
+        found = true;
+        break;
+      }
+    }
+    if (found === false) {
+      await Campus.create(campusAPI)
+      );
     }
   });
 }
